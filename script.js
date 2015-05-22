35744f042c079022b833dbbd6cfa97b06641b14b
Jenny Knuth <inkboco@gmail.com>
2015-05-21 20:39:12 -0600

    cleaned things up

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var greeting = document.getElementById('greeting');
 var cry = document.getElementById('cry')
 var goRight = true;
 var step = 23;
-var girth = 17;
+var grow = 17;
 
 
 var init = function (obj){
@@ -16,14 +16,15 @@ var init = function (obj){
 var move = function(){
 
   if (parseInt(man.style.left) < (window.innerWidth - 500) && goRight === true){
-    man.style.left = parseInt(man.style.left) + step + "px";
-    man.style.width = man.width + girth + "px";
+    man.style.left = parseInt(man.style.left) + step + 'px';
+    man.style.width = man.width + grow + 'px';
     if (parseInt(man.style.left) > step){
       greeting.innerHTML = "";
     }
     if (parseInt(man.style.left) > (window.innerWidth - (500 + step)) && parseInt(man.style.left) < (window.innerWidth - 500)){
-      cry.style.left = parseInt(man.style.left) + 400 + "px";
-      cry.innerHTML = "OUCH!";
+      cry.style.top = '100px';
+      cry.style.left = parseInt(man.style.left) + 400 + 'px';
+      cry.innerHTML = 'OUCH!';
     }
   }
   else if (parseInt(man.style.left) <= -150 && goRight === false){
@@ -31,25 +32,23 @@ var move = function(){
     man.style.transform = 'scaleX(1)';
     man.style.top = '100px';
     greeting.style.top = '25px';
-    greeting.innerHTML = "YooHoo!!!";
+    greeting.innerHTML = 'YooHoo!!!';
 
   }
   else{
     goRight = false;
-    man.style.transform = "scaleX(-1)";
-    man.style.left = parseInt(man.style.left) - step + "px";
-    man.style.width = man.width - girth + "px";
-    man.style.top = parseInt(man.style.top) + 10 + "px";
+    man.style.transform = 'scaleX(-1)';
+    man.style.left = parseInt(man.style.left) - step + 'px';
+    man.style.width = man.width - grow + 'px';
+    man.style.top = parseInt(man.style.top) + 10 + 'px';
     cry.innerHTML = "";
   }
 };
 
 init(man);
 //init text:
-greeting.style.position = "relative";
-cry.style.position = "relative";
-
-cry.style.top = "100px";
+greeting.style.position = 'relative';
+cry.style.position = 'relative';
 
 setInterval(move, 220);
 
