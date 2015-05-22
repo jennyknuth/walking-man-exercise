c53c2059737a95defe80a846b3f5a79e6357e828
Jenny Knuth <inkboco@gmail.com>
2015-05-21 19:49:37 -0600

    changes size and height, working on text

--- script.js
+++ script.js
@@ -1,36 +1,49 @@
 var man = document.getElementById('man');
+var greeting = document.getElementById('greeting');
+var cry = document.getElementById('cry')
 var goRight = true;
-//var position = man.style.left;
-//  var windowSize = window.innerWidth - 200px;
-//console.log(man.style.left);
 
 
 var init = function (obj){
   obj.style.position = 'relative';
   obj.style.left = '-100px';
-  obj.style.top = '200px';
+  obj.style.top = '100px';
+  obj.style.width = '100px';
 }
 
-console.dir(frame);
 var move = function(){
 
-  if (parseInt(man.style.left) < (document.body.offsetWidth - 200) && goRight === true){
+  if (parseInt(man.style.left) < (window.innerWidth - 500) && goRight === true){
     man.style.left = parseInt(man.style.left) + 23 + "px";
-    console.log(man.style.left);
+    man.style.width = man.width + 17 + "px";
+    if (parseInt(man.style.left) > 100){
+      greeting.innerHTML = "";
+    }
+    if (parseInt(man.style.left) > (window.innerWidth - 550) && parseInt(man.style.left) < (window.innerWidth - 500)){
+      cry.innerHTML = "OUCH!";
+    }
   }
   else if (parseInt(man.style.left) <= -100 && goRight === false){
     goRight = true;
     man.style.transform = "scaleX(1)";
+    man.style.top = '100px';
+    greeting.innerHTML = "YooHoo!!!";
+    cry.innerHTML = "";
   }
   else{
+
     goRight = false;
     man.style.transform = "scaleX(-1)";
     man.style.left = parseInt(man.style.left) - 23 + "px";
+    man.style.width = man.width - 17 + "px";
+    man.style.top = parseInt(man.style.top) + 10 + "px";
   }
 };
 
 init(man);
-
-setInterval(move, 220)
+// greeting.style.top = "50px";
+// cry.style.top = "50px";
+// cry.style.left = window.innerWidth - 50px;
+setInterval(move, 220);
 
 //man.addEventListener('click', move);
